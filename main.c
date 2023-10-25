13ab53d3266dd167bc821f373adc2584274ad656
Michael Antoš <antosmichael07@gmail.com>
2023-10-25 18:15:49 +0200

    commit die

--- main.c
+++ main.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int charArrayLength();
+#include <string.h>
 
 int main() {
 
     char installOption[1024];
+    char javaInstallOption[1024];
     char directory[1024];
-    char cdCommand[1024] = "cd ";
 
-    printf("Where do you want to install the mods?\n   A - Default minecraft directory\n   B - This directory\n   C - Custom directory\nExit - Enter anything else to exit\n");
+    printf("%s", "Where do you want to install the mods?\n   A - Default minecraft directory ( \%APPDATA\% -> .minecraft )\n   B - This directory\n   C - Custom directory ( It has to be already made )\nExit - Enter anything else to exit\n");
     scanf("%s", &installOption);
 
-    if (charArrayLength(installOption, sizeof(installOption)) > 1) {
+    if (strlen(installOption) > 1) {
         return 0;
     } else if (installOption[0] == 'A' || installOption[0] == 'a') {
-        char temp[1024] = "\%APPDATA\%\\.minecraft";
-
-        for (int i = 0; i < 20; i++) {
-            directory[i] = temp[i];
-        }
+        strcat(directory, "\%APPDATA\%\\.minecraft");
     } else if (installOption[0] == 'B' || installOption[0] == 'b') {
         directory[0] = ' ';
     } else if (installOption[0] == 'C' || installOption[0] == 'c') {
@@ -28,32 +23,42 @@ int main() {
         return 0;
     } 
 
-    for (int i = 0; i < charArrayLength(directory, sizeof(directory)); i++) {
-        cdCommand[i + 3] = directory[i];
+    printf("Do you have java installed?\nY - Yes\nN - No\n");
+    scanf("%s", &javaInstallOption);
+
+    if (strlen(javaInstallOption) > 1) {
+        return 0;
+    } else if (javaInstallOption[0] == 'Y' || javaInstallOption[0] == 'y') {
+        printf("");
+    } else if (javaInstallOption[0] == 'N' || javaInstallOption[0] == 'n') {
+        printf("");
+    } else {
+        return 0;
     }
 
-    system(cdCommand);
-    system("mkdir mods");
-    system("cd mods");
-    system("powershell");
-    system("Invoke-WebRequest -Uri \"https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.2-43.3.2/forge-1.19.2-43.3.2-installer.jar\" -OutFile \"forge-1.19.2-43.3.2-installer.jar\"");
-    system("exit");
-    system("start \"forge-1.19.2-43.3.2-installer.jar\"");
+    char batFileDestination[1024] = "";
+    strcat(batFileDestination, directory);
+    strcat(batFileDestination, "\\mods\\downloader.bat");
+
+    char cmdCommand1[1024] = "echo powershell >> ";
+    strcat(cmdCommand1, batFileDestination);
+    system(cmdCommand1);
+
+    char cmdCommand2[1024] = "echo Invoke-WebRequest -Uri \"https://www.dropbox.com/scl/fi/mkldyx7lv90scoowxd839/mods.zip?rlkey=8hd42awzi5szq96aovb8ocu6r&dl=1\" OutFile \"mods.zip\" >> ";
+    strcat(cmdCommand2, batFileDestination);
+    system(cmdCommand2);
+
+    char cmdCommand3[1024] = "echo exit >> ";
+    strcat(cmdCommand3, batFileDestination);
+    system(cmdCommand3);
+
+    char cmdCommand4[1024] = "echo tar -xf mods.zip >> ";
+    strcat(cmdCommand4, batFileDestination);
+    system(cmdCommand4);
+
+    char cmdCommand5[1024] = "start ";
+    strcat(cmdCommand5, batFileDestination);
+    system(cmdCommand5);
 
     return 0;
-}
-
-int charArrayLength(char array[], int arrayLength) {
-
-    int temp = 0;
-
-    for (int i = 0; i < arrayLength; i++) {
-        if (array[i] == '\0') {
-            break;
-        }
-
-        temp++;
-    }
-
-    return temp;
 }
